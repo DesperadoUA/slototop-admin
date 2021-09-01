690ebdb75233570e0984d808bf011a5894a71d19
lazarev <k.lazarev@cleack.com>
2021-09-01 14:36:06 +0300

    Add Static pages

--- DAL/pages.js
+++ DAL/pages.js
@@ -2,13 +2,13 @@ import axios from 'axios'
 import config from './config';
 class DAL_Pages {
     static getPages(data) {
-        return axios.post(config.API_URL + 'admin/pages', data)
+        return axios.post(config.API_URL + 'pages', data)
     }
     static getPageById(data) {
-        return axios.post(config.API_URL + `admin/pages/${data.url}`, data)
+        return axios.post(config.API_URL + `pages/${data.url}`, data)
     }
     static updatePage(data) {
-        return axios.post(config.API_URL + `admin/pages/update`, data)
+        return axios.post(config.API_URL + `pages/update`, data)
     }
 }
 export default DAL_Pages
